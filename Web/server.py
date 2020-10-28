d49651f090c752f6ea08c9f54e1c4b69cd0c1507
cabadilla <cabadilla@ic-itcr.ac.cr>
2020-10-27 22:29:19 -0600

    pruebas_para_pasar_datos

--- Web/server.py
+++ Web/server.py
@@ -1,11 +1,18 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,request
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.static_folder = 'static'
 @app.route('/')
-def login():
+def login(methods=['GET']):
     return render_template('login.html')
+
+@app.route('/nombres',methods=['POST'])
+def nombres():
+    nombre = request.form['u']
+    contrasena = request.form['p']
+    return 'Hola ' + nombre + ' ' + contrasena
+
 
 app.run(port=3000, debug=True)
