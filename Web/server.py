20490432e45ba2d8441f7c0d51420ecf1f482099
cabadilla <cabadilla@ic-itcr.ac.cr>
2020-10-31 16:59:36 -0600

    se_anaden_nuevas_rutas

--- Web/server.py
+++ Web/server.py
@@ -1,18 +1,41 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask,render_template,url_for,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.static_folder = 'static'
+
+#ruta de inicio donde se hace el login
 @app.route('/')
-def login(methods=['GET']):
+def login():
     return render_template('login.html')
 
-@app.route('/nombres',methods=['POST'])
-def nombres():
+#se entra a la sesion del usuario correcto y se validan los datos
+@app.route('/entrar',methods=['POST'])
+def entrar():
     nombre = request.form['u']
     contrasena = request.form['p']
-    return 'Hola ' + nombre + ' ' + contrasena
+    return redirect('/main')
+
+#ruta del menu principal
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+#ruta de los beneficiarios
+@app.route('/beneficiario')
+def beneficiario():
+    return render_template('beneficiario.html')
+
+#ruta de los estados de cuenta
+@app.route('/estadosDeCuenta')
+def estadosDeCuenta():
+    return render_template('estadosDeCuenta.html')
+
+#ruta para ver la informacion del cliente
+@app.route('/informacionPersonal')
+def informacionPersonal():
+    return render_template('informacionPersonal.html')
 
 
 app.run(port=3000, debug=True)
